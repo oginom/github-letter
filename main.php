caf57fe3ab48c0886cf243f50849931cdc9a16b4
oginom <ma@ogino-no-MacBook-Pro.local>
2017-10-05 14:49:33 +0900

    update

--- main.php
+++ main.php
@@ -1,17 +1,41 @@
 <?php
   date_default_timezone_set('Asia/Tokyo');
-  $d = date(z);
+  $w = date("w");
+  
+  $mat = array(
+    array(3, 3, 3, 3, 3, 3, 3),
+    array(3, 0, 3, 0, 3, 0, 3),
+    array(3, 3, 3, 3, 3, 3, 3),
+    array(3, 0, 3, 0, 3, 0, 3),
+    array(3, 3, 3, 3, 3, 3, 3),
+    array(3, 0, 3, 0, 3, 0, 3),
+    array(3, 3, 3, 3, 3, 3, 3)
+  );
+  
   $f = file("lastupdate.txt");
   var_dump($f);
-  $a = $b = 0;
-  try {
-    $a = intval($f[0]);
-    $b = intval($f[1]);
-  } catch(Exception $e) {
-    echo "error\n";
+  if(count($f) != 3) {
+    $f = array("0", "0", "0");
   }
-  file_put_contents("lastupdate.txt", $d);
-  exec("git add lastupdate.txt", $outpara);
-  exec("git commit -a -m 'update'", $outpara);
-  exec("git push origin master", $outpara);
+  $f_l = intval($f[0]);
+  $f_w = intval($f[1]);
+  $f_c = intval($f[2]);
+  if($f_w != $w) {
+    if($f_w > $w) {
+      $f_l = $f_l + 1;
+      if($f_l >= count($mat)) {
+        $f_l = 0;
+      }
+    }
+    $f_w = $w;
+    $f_c = 0;
+  }
+  if($f_c < $mat[$f_l][$f_w]) {
+    $f_c = $f_c + 1;
+    $s = $f_l."\n".$f_w."\n".$f_c."\n";
+    file_put_contents("lastupdate.txt", $s);
+    exec("git add lastupdate.txt", $outpara);
+    exec("git commit -a -m 'update'", $outpara);
+    exec("git push origin master", $outpara);
+  }
 ?>
