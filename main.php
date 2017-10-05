a0aa6ccb0f2716155aefdf5e0010e5911ece0792
oginom <ma@ogino-no-MacBook-Pro.local>
2017-10-05 14:00:03 +0900

    update

--- main.php
+++ main.php
@@ -1,3 +1,11 @@
 <?php
-
+  //if(!file_exists('')) {
+  //}
+  $f = file("lastupdate.txt");
+  var_dump($f);
+  $d = date();
+  file_put_contents("lastupdate.txt", $d);
+  exec("git add lastupdate.txt", $outpara);
+  exec("git commit -a -m 'update'", $outpara);
+  exec("git push origin master", $outpara);
 ?>
